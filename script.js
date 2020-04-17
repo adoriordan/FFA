fe8cea6bee6bfb475d94de9367b80644438252ac
btracy2 <brian_tracy@brown.edu>
2020-04-17 16:29:03 -0700

    script is now pulling from farm_data.csv

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
-window.addEventListener('DOMContentLoaded', function() {
-    console.log('script loaded');
+function csv_loaded(data) {
+    document.write(data);
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('farm_data.csv')
+    .then((resp) => resp.text())
+    .then(csv_loaded);
 });
 
