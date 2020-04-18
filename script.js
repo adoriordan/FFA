b535807766187455729c0a1f5f1c10edae6f6a7a
btracy2 <brian_tracy@brown.edu>
2020-04-17 22:03:53 -0700

    added code to support direct fetch from google sheets api

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 
-function csv_loaded(data) {
-    //document.write(data);
-    console.log(data);
-}
-
-function fetch_failed(err) {
-    console.log(err);
-    document.write(err)
-}
-
 window.addEventListener('DOMContentLoaded', () => {
-    fetch('farm_data.csv')
-    .then((resp) => resp.text())
-    .then(csv_loaded)
+    const sheet_key = '1z_gwSbVnzwlKHi4CPJ0UMVjRERR2D15jgcId5agQTEU';
+    const page_num = '1';
+    const endpoint = `https://spreadsheets.google.com/feeds/cells/${sheet_key}/${page_num}/public/full?alt=json`;
+
+    fetch(endpoint)
+    .then((resp) => resp.json())
+    .then(data_loaded)
     .catch(fetch_failed);
 });
 
 //////////////////////////////////////
 //////////////////////////////////////
 
+function fetch_failed(err) {
+    alert(err);
+}
+
+function data_loaded(data) {
+    console.log(data);
+}
 
