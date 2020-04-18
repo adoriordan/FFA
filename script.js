861bd33340cdf47d34b805589084d7ab49197c01
btracy2 <brian_tracy@brown.edu>
2020-04-17 17:04:39 -0700

    automated commit [Fri Apr 17 17:04:39 PDT 2020]

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 
 function csv_loaded(data) {
-    document.write(data);
+    //document.write(data);
+    console.log(data);
+}
+
+function fetch_failed(err) {
+    console.log(err);
+    document.write(err)
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     fetch('farm_data.csv')
     .then((resp) => resp.text())
-    .then(csv_loaded);
+    .then(csv_loaded)
+    .catch(fetch_failed);
 });
+
+//////////////////////////////////////
+//////////////////////////////////////
+
 
